c8837df86a9118b15167d9e01f14bce0b1ebe2a6
TcpAckFrequency <nezpower@abv.bg>
2019-11-12 18:35:30 +0200

    Add JWT

--- src/main/java/me/tcpackfrequency/Warehouse/WarehouseApplication.java
+++ src/main/java/me/tcpackfrequency/Warehouse/WarehouseApplication.java
@@ -2,11 +2,13 @@ package me.tcpackfrequency.Warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class WarehouseApplication {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		SpringApplication.run(WarehouseApplication.class, args);
 	}
 }
