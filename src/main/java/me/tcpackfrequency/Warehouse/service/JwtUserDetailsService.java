676ca964fcc994c3ea65cb85460aaa312cf3b7a2
TcpAckFrequency <nezpower@abv.bg>
2019-11-30 11:48:47 +0200

    Finish JWT & Postgres set up.

--- src/main/java/me/tcpackfrequency/Warehouse/service/JwtUserDetailsService.java
+++ src/main/java/me/tcpackfrequency/Warehouse/service/JwtUserDetailsService.java
@@ -1,8 +1,9 @@
 package me.tcpackfrequency.Warehouse.service;
 
+import me.tcpackfrequency.Warehouse.config.WebSecurityConfig;
+import me.tcpackfrequency.Warehouse.model.User;
 import me.tcpackfrequency.Warehouse.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,19 +15,23 @@ import java.util.ArrayList;
 public class JwtUserDetailsService implements UserDetailsService {
 
 	@Autowired
+	WebSecurityConfig security;
+
+	@Autowired
 	UserRepository repository;
+
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		for(String s : repository.findAllNames()){
-			if(username.equals(s)){
-				return new User(s, "$2y$12$leBE7q5iewAJVO2KsuBlmOqkq/e1rs.Va6JLA5u2o90WwaSwSG/ii", // test
-				new ArrayList<>());
+		for(User s : repository.findAll()){
+			if(s.getUSER_NAME().equals(username)){
+				return new org.springframework.security.core.userdetails.User(s.getUSER_NAME(), security.passwordEncoder().encode(s.getUSER_PASSWORD()), new ArrayList<>()
+				);
 			}
 		}
 
 			if ("asdf".equals(username)) {
-				return new User("asdf", "$2y$12$leBE7q5iewAJVO2KsuBlmOqkq/e1rs.Va6JLA5u2o90WwaSwSG/ii",
+				return new org.springframework.security.core.userdetails.User("asdf", "$2y$12$leBE7q5iewAJVO2KsuBlmOqkq/e1rs.Va6JLA5u2o90WwaSwSG/ii",
 						new ArrayList<>());
 			} else {
 				// why doesnt it throw this exception?
